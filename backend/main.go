5551a73ba42ab24679f2e4d37cbe67302bc74e51
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2025-04-20 18:33:31 -0700

    Add backend

---
+++ backend/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/chi"
+	"github.com/go-chi/chi/v5"
+)
+
+var chiLambda *chiadapter.ChiLambda
+
+func init() {
+	r := chi.NewRouter()
+
+	// Define routes
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from Go-chi!"))
+	})
+
+	r.Get("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the API!"))
+	})
+
+	// Wrap chi router with AWS Lambda adapter
+	chiLambda = chiadapter.New(r)
+}
+
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return chiLambda.Proxy(req)
+}
+
+func main() {
+	lambda.Start(handler)
+}
