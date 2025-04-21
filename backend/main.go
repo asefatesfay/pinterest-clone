4d7098aec4f55f5a3ba683ffb4d286e4f12105ba
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2025-04-20 19:28:54 -0700

    Fix pipeline

--- backend/main.go
+++ backend/main.go
@@ -20,7 +20,7 @@ func init() {
 	})
 
 	r.Get("/api", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to the API!"))
+		w.Write([]byte("Welcome to the API-1!"))
 	})
 
 	// Wrap chi router with AWS Lambda adapter
