2da35cce5b9b5765ebd4060b36630edaaa9ec1ca
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2025-04-22 17:37:38 -0700

    change backend from golang to nodejs

--- backend/go.mod
+++
@@ -1,9 +0,0 @@
-module backend
-
-go 1.24.1
-
-require (
-	github.com/aws/aws-lambda-go v1.48.0
-	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
-	github.com/go-chi/chi/v5 v5.2.1
-)
---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const serverless = require("aws-lambda-ric");
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello from Express on AWS Lambda!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to the API!" });
+});
+
+// Export the app as a Lambda handler
+exports.handler = serverless.express(app);
