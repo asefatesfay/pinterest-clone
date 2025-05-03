7429f691d5f24b27a6734e7eb625182891e1b9a0
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2025-05-03 11:16:35 -0700

    Fix pipeline

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send("Hello from Express-v1!");
 });
 
 app.get("/api", (req, res) => {
