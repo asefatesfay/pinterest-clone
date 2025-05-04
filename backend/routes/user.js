b97c5a2c81eb1d407de3a577e363c38393dd6373
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2025-05-03 19:26:59 -0700

    Add schema

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,11 @@ import {getUser} from "../controllers/user.js";
 
 const router = express.Router();
 
+router.post("/create", (req, res) => {
+    const userInfo = req.body;
+    console.log(userInfo);
+});
+
 router.get("/test", getUser);
 
 export default router;
